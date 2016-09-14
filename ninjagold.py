bcb4b239de2c3cb27dab98b8d067373f3d074588
Jonathan Ben-Ammi <jbenammi@gmail.com>
2016-09-14 14:10:09 -0700

    Code Review Comments about Ninja Gold assignment
    Over all good job on the assignment. 
    Take a look over the comments in your code and let me know if you have questions.
    Suggest watching your use of session. It is very useful and can be over used when not strictly needed. 
    I liked how you came up with changing the colors of your text based on what was being passed in, nice work.

--- ninjagold.py
+++ ninjagold.py
@@ -5,8 +5,12 @@ import random
 app = Flask(__name__)
 app.secret_key = 'mykey'
 
+// Is 'post' nessesary for this route?
 @app.route('/', methods=['get','post'])
 def njgld():
+    // Comments: There is alot of use of session for holding variable data globaly that isn't nessesary. Should think about session as a
+    // a variable space that can be accessed from other pages and will retain it's data regardless of what file you are accessing. If you
+    // do not need to do a running save of date (You do for gold and log so these are good) then you should probably use local variables instead. (time, building and delta could all have been done using local variables.
     session['log'] = []
     session['state'] = None
     session['time'] = None
@@ -36,6 +40,7 @@ def guess():
         session['building'] = 'casino'
     session['time'] = datetime.now()
     session['log'].append((session['building'],session['delta'],session['time']))
+    // Any time you have more than one route rendering the same template, you should stop and think if this is nessesary. In this case, the thought process is that when you are done with the code above, you will then redirect to '/' in order to render your index HTML. The same will hold true for your '/reset' route.
     return render_template('index.html')
 
 @app.route('/reset', methods=['post'])
